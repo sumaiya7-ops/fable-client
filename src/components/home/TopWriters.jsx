4ff5d89ccade5bffafee1f66bc2f52e5c4fc5bdb
sumaiya7-ops <sumaiya7-ops@gmail.com>
2026-07-01 15:26:17 +0600

    add

--- src/components/home/TopWriters.jsx
+++ src/components/home/TopWriters.jsx
@@ -1,37 +1,21 @@
 "use client";
 
 import React from "react";
-// 🟢 নেভিগেশনের জন্য Next.js Link ইমপোর্ট করা হলো
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const writers = [
-  {
-    id: 1,
-    name: "William Shakespeare",
-    books: "4.0B+ Copies Referenced",
-    image: "https://upload.wikimedia.org/wikipedia/commons/a/a2/Shakespeare.jpg",
-  },
-  {
-    id: 2,
-    name: "J. K. Rowling",
-    books: "600M+ Books Sold",
-    image: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=500",
-  },
-  {
-    id: 3,
-    name: "Agatha Christie",
-    books: "2B+ Books Sold",
-    image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=500",
-  },
-  {
-    id: 4,
-    name: "Stephen King",
-    books: "400M+ Books Sold",
-    image: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=500",
-  },
-];
 
 export default function TopWriters() {
+  const [writers, setWriters] = useState([]);
+
+useEffect(() => {
+  axios
+    .get("https://fable-server-z2xt.onrender.com/writers/top")
+    .then((res) => {
+      setWriters(res.data);
+    });
+}, []);
   return (
     <section className="bg-indigo-100 mt-20 mb-20 w-full flex justify-center py-16">
       <div className="w-11/12 md:w-10/12 max-w-7xl">
@@ -57,14 +41,14 @@ export default function TopWriters() {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8"  >
           {writers.map((writer) => (
             <div
-              key={writer.id}
+              key={writer._id}
               className="group bg-white rounded-2xl p-6 shadow-md hover:shadow-2xl hover:-translate-y-2 transition-all duration-300"
             style={{ padding: "6px"  }}
             >
               {/* Image */}
               <div className="relative overflow-hidden rounded-2xl">
                 <img
-                  src={writer.image}
+                src={writer.avatar}
                   alt={writer.name}
                   className="w-full h-64 object-cover transition duration-500 group-hover:scale-105"
                 />
@@ -77,7 +61,7 @@ export default function TopWriters() {
                 </h3>
 
                 <p className="text-gray-500 mt-2">
-                  {writer.books}
+                  {writer.totalSales} Sales
                 </p>
 
                 {/* 🟢 button কেটে রিকোয়ারমেন্ট অনুযায়ী Link ট্যাগ দেওয়া হলো যেন প্রোফাইল পেজে নিয়ে যায় */}
